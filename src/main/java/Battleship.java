70e0cf9066ba5bc66ce6b5058c0e91c612b2264e
vevdokimenko <evdokimenko@hotmail.com>
2021-11-08 10:40:18 +0200

    Change encoding to utf-8

--- src/main/java/Battleship.java
+++ src/main/java/Battleship.java
@@ -1,20 +1,12 @@
-import characters.Player;
-import characters.Ship;
 import utils.Splashscreen;
 
-import java.io.PrintStream;
-import java.io.UnsupportedEncodingException;
-import java.util.List;
-
 public class Battleship {
-    public static void main(String[] args) throws UnsupportedEncodingException {
-        System.setOut(new PrintStream(System.out, true, "utf-8"));
+    public static void main(String[] args) {
+//        Player player = new Player("");
+//        player.fillPlayerField();
 
-        Player player = new Player("");
-        player.fillPlayerField();
-
-//        new Splashscreen();
-//        new Gameplay();
+        new Splashscreen();
+        new Gameplay();
     }
 }
 
